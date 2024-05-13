612177b2b63d570f43d0cf6cf0aef8deab7858f5
summrh4ze <todor.andrei.94@gmail.com>
2024-05-13 11:34:00 +0300

    exit when client could not connect to server

--- client/client.go
+++ client/client.go
@@ -3,12 +3,14 @@ package main
 import (
 	ws "example/zerochat/websocket"
 	"fmt"
+	"os"
 )
 
 func main() {
 	conn, err := ws.Connect("ws://127.0.0.1:8080/chat")
 	if err != nil {
 		fmt.Printf("Error connecting to websocket: %s\n", err)
+		os.Exit(1)
 	}
 
 	defer conn.Close()
